4178e51984cff3bf4a836ff4bb397a30339f0df7
slam24 <luismartinez242011@gmail.com>
2017-04-11 12:27:44 -0600

    feat: Create component

--- app/dist/index.js
+++ app/dist/index.js
@@ -1,2 +1,23 @@
 require('./module1.js');
 require('./module2.js');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Layout extends React.Component{
+	constructor(){
+		super();
+		this.nombre = "pepito";		
+	}
+	getName(){
+		return 'hi';
+	}
+    render(){
+        return(
+            <h1>Hello { this.nombre } { this.getName() }</h1>
+        );
+    }
+}
+
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>,app);
