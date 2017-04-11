481f5f6a5c935691978d464895f0269f1224ef66
slam24 <luismartinez242011@gmail.com>
2017-04-11 17:19:07 -0600

    feat: Events & data changes

--- app/src/components/FilterableProductTable.js
+++ app/src/components/FilterableProductTable.js
@@ -6,12 +6,25 @@ import ProductTable from './ProductTable.js'
 export default class FilterableProductTable extends React.Component{
 	constructor(){
 		super()
+		this.state = {
+			filter: null
+		}
 	}
+
+	filterList(ev){
+		let filter = ev.target.value
+		setTimeout(()=>{
+			this.setState({
+				filter:filter
+			})
+		},100)
+	}
+
 	render(){
 		return(
 			<div>
-				<SearchBar/>
-				<ProductTable products={this.props.store} />
+				<SearchBar onChange={this.filterList.bind(this)}/>
+				<ProductTable products={this.props.store} filter={this.state.filter}/>
 			</div>
 			)
 	}
--- app/src/components/ProductTable.js
+++ app/src/components/ProductTable.js
@@ -10,7 +10,17 @@ export default class ProductTable extends React.Component{
 	render(){
 		let rows = []
 		let last_category = null
-		if(this.props.products != null){
+		if(this.props.filter != null && this.props.products != null){
+			this.props.products.forEach((product) =>{
+				let filter = this.props.filter
+				if (product.name.indexOf(filter) > -1) {
+					if (product.category != last_category)
+						rows.push(<ProductCategoryRow category={product.category} key={product.category}/>)
+					rows.push(<ProductRow product={product} key={product.name}/>)
+					last_category = product.category
+				}
+			})
+		}else if(this.props.products != null){
 			this.props.products.forEach((product) => {
 				if (product.category != last_category)
 					rows.push(<ProductCategoryRow category={product.category} key={product.category}/>)
